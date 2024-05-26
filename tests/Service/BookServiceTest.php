46d4fc50b6c1e16c3f94367e2ac6dd903e4f0029
Diana Dvoryak <dianadvoryak.w@gmail.com>
2024-05-26 10:15:08 +0200

    testGetBooksByCategory

---
+++ tests/Service/BookServiceTest.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Tests\Service;
+
+use App\Entity\Book;
+use App\Entity\BookCategory;
+use App\Exception\BookCategoryNotFoundException;
+use App\Model\BookListItem;
+use App\Model\BookListResponse;
+use App\Repository\BookCategoryRepository;
+use App\Repository\BookRepository;
+use App\Service\BookService;
+use App\Tests\AbstractTestCase;
+use Doctrine\Common\Collections\ArrayCollection;
+use PHPUnit\Framework\TestCase;
+
+class BookServiceTest extends AbstractTestCase
+{
+
+    public function testGetBooksByCategoryNotFound()
+    {
+        $bookRepository = $this->createMock(BookRepository::class);
+        $bookCategoryRepository = $this->createMock(BookCategoryRepository::class);
+        $bookCategoryRepository->expects($this->once())
+            ->method('find')
+            ->with(130)
+            ->willThrowException(new BookCategoryNotFoundException());
+
+        $this->expectException(BookCategoryNotFoundException::class);
+
+        (new BookService($bookRepository, $bookCategoryRepository))->getBooksByCategory(130);
+    }
+
+    public function testGetBooksByCategory(): void
+    {
+        $bookRepository = $this->createMock(BookRepository::class);
+        $bookRepository->expects($this->once())
+            ->method('findBooksByCategoryId')
+            ->with(130)
+            ->willReturn([$this->createBookEntity()]);
+
+        $bookCategoryRepository = $this->createMock(BookCategoryRepository::class);
+        $bookCategoryRepository->expects($this->once())
+            ->method('find')
+            ->with(130)
+            ->willReturn(new BookCategory());
+
+        $service = new BookService($bookRepository, $bookCategoryRepository);
+        $expected = new BookListResponse([$this->createBookItemModel()]);
+
+        $this->assertEquals($expected, $service->getBooksByCategory(130));
+    }
+
+    private function createBookEntity(): Book
+    {
+        $category = (new BookCategory())->setTitle('Category')->setSlug('category');
+        $this->setEntityId($category, 1);
+
+        $book = (new Book())
+            ->setTitle('Test Book')
+            ->setSlug('test-book')
+            ->setMeap(false)
+//            ->setIsbn('123321')
+//            ->setDescription('test description')
+            ->setAuthor(['Tester'])
+            ->setImage('http://localhost/test.png')
+            ->setCategories(new ArrayCollection([$category]))
+            ->setPublicationDate(new \DateTimeImmutable('2020-10-10'));
+//            ->setFormats(new ArrayCollection([$join]));
+
+        $this->setEntityId($book, 123);
+
+        return $book;
+    }
+
+    private function createBookItemModel(): BookListItem
+    {
+        return (new BookListItem())
+            ->setId(123)
+            ->setTitle('Test Book')
+            ->setSlug('test-book')
+            ->setMeap(false)
+            ->setAuthor(['Tester'])
+            ->setImage('http://localhost/test.png')
+            ->setPublicationDate(1602288000);
+    }
+}
