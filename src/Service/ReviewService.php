dfa7660838c18bf2fa79416eb97789796c6f0780
Diana Dvoryak <dianadvoryak.w@gmail.com>
2024-06-02 10:16:50 +0200

    refactoring

--- src/Service/ReviewService.php
+++ src/Service/ReviewService.php
@@ -11,7 +11,7 @@ class ReviewService
 {
     private const PAGE_LIMIT = 5;
 
-    public function __construct(private ReviewRepository $reviewRepository)
+    public function __construct(private ReviewRepository $reviewRepository, private RatingService $ratingService)
     {
     }
 
@@ -22,14 +22,8 @@ class ReviewService
         $ratingSum = $this->reviewRepository->getBookTotalRatingSum($id);
         $total = count($paginator);
 
-        $rating = 0;
-
-        if ($total > 0) {
-            $rating = $this->reviewRepository->getBookTotalRatingSum($id) / $total;
-        }
-
         return (new ReviewPage())
-            ->setRating($rating)
+            ->setRating($this->ratingService->calcReviewRatingForBook($id, $total))
             ->setTotal($total)
             ->setPage($page)
             ->setPerPage(self::PAGE_LIMIT)
